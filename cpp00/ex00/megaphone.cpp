e769a0cbdeabff02de849f6dbffd3fcfb1c9b445
Lucas Pires Nogueira <piresnogueiralucas@gmail.com>
2023-04-25 21:45:58 -0300

    done ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+        {
+            std::cout << (char)std::toupper(argv[i][j]);
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
