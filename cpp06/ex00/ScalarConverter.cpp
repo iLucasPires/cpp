31dcf0fe86b14cdff0520edc3d6286388f31a371
Lucas Pires Nogueira <piresnogueiralucas@gmail.com>
2023-06-16 21:39:32 -0300

    add type impressions

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,6 +3,7 @@
 ScalarConverter::ScalarConverter() {
   std::cout << "Default constructor called" << std::endl;
 }
+
 ScalarConverter::ScalarConverter(ScalarConverter const &src) {
   std::cout << "Copy constructor called" << std::endl;
   *this = src;
@@ -18,43 +19,81 @@ ScalarConverter &ScalarConverter::operator=(ScalarConverter const &rhs) {
   return *this;
 }
 
-void ScalarConverter::convert(std::string const str) {
-  typeState type = verifyInput(str);
+void ScalarConverter::convert(std::string const &str) {
   dataType data;
 
-  if (type == CHAR) {
-    std::cout << "type: " << type << '\n';
+  data.t = verifyInput(str);
+  data.p = str.find('.');
+
+  if (data.p != std::string::npos) {
+    data.p = str.length() - data.p - 1;
+    if (data.t == FLOAT) data.p -= 1;
+  }
+   else {
+    data.p = 1;
+  }
+
+  conversion(data, str);
+  conversionPrint(data);
+}
+
+void ScalarConverter::conversion(dataType &data, std::string const &str) {
+  if (data.t == CHAR) {
     data.c = static_cast<char>(str[0]);
     data.i = static_cast<int>(data.c);
     data.f = static_cast<float>(data.c);
     data.d = static_cast<double>(data.c);
-  } else if (type == INT) {
+  } else if (data.t == INT) {
     data.i = static_cast<int>(std::atol(str.c_str()));
     data.c = static_cast<char>(data.i);
     data.f = static_cast<float>(data.i);
     data.d = static_cast<double>(data.i);
-  } else if (type == FLOAT) {
+  } else if (data.t == FLOAT) {
     data.f = static_cast<float>(std::atof(str.c_str()));
     data.i = static_cast<int>(data.f);
     data.d = static_cast<double>(data.f);
     data.c = static_cast<char>(data.f);
-  } else if (type == DOUBLE) {
+  } else if (data.t == DOUBLE) {
     data.d = static_cast<double>(std::strtod(str.c_str(), NULL));
     data.i = static_cast<int>(data.d);
     data.f = static_cast<float>(data.d);
     data.d = static_cast<double>(data.d);
   }
-  else if (type == INVALID)
-  {
-    std::cout << "Invalid input, please to use CHAR, INT, FLOAT or DOUBLE" << std::endl;
-    return;
+}
+
+void ScalarConverter::conversionPrint(dataType &data) {
+  std::cout << "char: ";
+  if (IS_NON_DISPLAYABLE(data.c) && IS_ASCII(data.c)) {
+    std::cout << "Non displayable\n";
+  } else if (IS_CHAR(data.c)) {
+    std::cout << "'" << data.c << "'\n";
+  } else {
+    std::cout << "impossible\n";
   }
 
+  std::cout << "int: ";
+  if (data.t != INVALID) {
+    std::cout << data.i << '\n';
+  } else {
+    std::cout << "impossible\n";
+  }
 
-  std::cout << "char: " << (IS_NON_DISPLAYABLE(data.c) ? "Non displayable" : std::string( 1, data.c));
+  std::cout << "float: ";
+  if (data.f > std::numeric_limits<float>::max() ||
+      data.f < std::numeric_limits<float>::min()) {
+    std::cout << "impossible\n";
+  } else if (data.t != INVALID) {
+    std::cout << std::fixed << std::setprecision(data.p) << data.f << 'f'
+              << '\n';
+  }
 
-
-  std::cout << std::endl;
+  std::cout << "double: ";
+  if (data.d > std::numeric_limits<double>::max() ||
+      data.d < std::numeric_limits<double>::min()) {
+    std::cout << "impossible\n";
+  } else if (data.t != INVALID) {
+    std::cout << std::fixed << std::setprecision(data.p) << data.d << '\n';
+  }
 }
 
 typeState ScalarConverter::verifyInput(const std::string &str) {
@@ -67,7 +106,7 @@ typeState ScalarConverter::verifyInput(const std::string &str) {
   if (length == 1 && !IS_INT(str[0])) return CHAR;
 
   while (index < length) {
-    while (IS_SIGN(str[index])) ++index;
+    if (IS_SIGN(str[index])) ++index;
     if (!IS_INT(str[index])) break;
     ++index;
   }
@@ -78,7 +117,8 @@ typeState ScalarConverter::verifyInput(const std::string &str) {
 
     while (index < length && IS_INT(str[index])) ++index;
 
-    if (str[index] == 'f' && index == length - 1) return FLOAT;
+    if ((str[index] == 'f' || str[index] == 'F') && index == length - 1)
+      return FLOAT;
     if (index == length) return DOUBLE;
   }
 
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -3,8 +3,8 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
-#include <string>
+#include <iomanip>
+#include <limits>
 
 enum typeState { CHAR, INT, FLOAT, DOUBLE, PSUEDOFLOAT, PSUEDODOUBLE, INVALID };
 
@@ -13,16 +13,19 @@ struct dataType {
   char c;
   float f;
   double d;
+  size_t  p;
+  typeState t;
 };
 
 #define IS_CHAR(c) ((c) >= 32 && (c) <= 126)
 #define IS_INT(c) ((c) >= 48 && (c) <= 57)
+#define IS_ASCII(c) ((c) >= 0 && (c) <= 127)
 #define IS_NON_DISPLAYABLE(c) ((c) < 32 || (c) > 126)
 #define IS_SIGN(c) ((c) == '-' || (c) == '+')
 
 class ScalarConverter {
  public:
-  static void convert(std::string const str);
+  static void convert(std::string const &str);
 
  private:
   ScalarConverter();
@@ -31,6 +34,8 @@ class ScalarConverter {
   ScalarConverter &operator=(ScalarConverter const &rhs);
 
   static typeState verifyInput(std::string const &str);
+  static void conversion(dataType &data, std::string const &str);
+  static void conversionPrint(dataType &data);
 };
 
 #endif
